5e3fd37d5f58bcb7c8e42e8ff329479fbe9c5afd
Christian Stewart <christian@aperture.us>
2025-04-22 14:18:47 -0700

    chore: minor cleanup
    Signed-off-by: Christian Stewart <christian@aperture.us>

--- cmd/goscript/cmd_compile.go
+++ cmd/goscript/cmd_compile.go
@@ -15,40 +15,38 @@ var cliCompilerPkg string
 var test1, test2 = 1, 2
 
 // CompileCommands are commands related to compiling code.
-var CompileCommands = []*cli.Command{
-	&cli.Command{
-		Name:     "compile",
-		Category: "compile",
-		Usage:    "compile a Go package to TypeScript",
-		Action:   compilePackage,
-		Before: func(c *cli.Context) (err error) {
-			logger := logrus.New()
-			logger.SetLevel(logrus.DebugLevel)
-			le := logrus.NewEntry(logger)
-			cliCompiler, err = compiler.NewCompiler(&cliCompilerConfig, le, nil)
-			return
+var CompileCommands = []*cli.Command{{
+	Name:     "compile",
+	Category: "compile",
+	Usage:    "compile a Go package to TypeScript",
+	Action:   compilePackage,
+	Before: func(c *cli.Context) (err error) {
+		logger := logrus.New()
+		logger.SetLevel(logrus.DebugLevel)
+		le := logrus.NewEntry(logger)
+		cliCompiler, err = compiler.NewCompiler(&cliCompilerConfig, le, nil)
+		return
+	},
+	Flags: []cli.Flag{
+		&cli.StringFlag{
+			Name:        "package",
+			Usage:       "the package to compile",
+			Destination: &cliCompilerPkg,
 		},
-		Flags: []cli.Flag{
-			&cli.StringFlag{
-				Name:        "package",
-				Usage:       "the package to compile",
-				Destination: &cliCompilerPkg,
-			},
-			&cli.StringFlag{
-				Name:        "gopath",
-				Usage:       "the gopath / go root to use",
-				Destination: &cliCompilerConfig.GoPathRoot,
-				EnvVars:     []string{"GOPATH"},
-			},
-			&cli.StringFlag{
-				Name:        "output",
-				Usage:       "the output typescript path to use",
-				Destination: &cliCompilerConfig.OutputPathRoot,
-				Value:       "./output",
-			},
+		&cli.StringFlag{
+			Name:        "gopath",
+			Usage:       "the go root to use",
+			Destination: &cliCompilerConfig.GoPathRoot,
+			EnvVars:     []string{"GOPATH"},
+		},
+		&cli.StringFlag{
+			Name:        "output",
+			Usage:       "the output typescript path to use",
+			Destination: &cliCompilerConfig.OutputPathRoot,
+			Value:       "./output",
 		},
 	},
-}
+}}
 
 // compilePackage tries to compile the package.
 func compilePackage(c *cli.Context) error {
