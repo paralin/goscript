ea918a2dfdfe1e0862f15cab9d6f695d9ea0d125
Christian Stewart <christian@aperture.us>
2025-04-25 18:41:48 -0700

    chore: add node globals
    Signed-off-by: Christian Stewart <christian@aperture.us>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,23 @@
 // @ts-check
 
-import eslint from '@eslint/js';
-import tseslint from 'typescript-eslint';
+import globals from 'globals'
+import eslint from '@eslint/js'
+import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
   eslint.configs.recommended,
   tseslint.configs.recommended,
   {
-    "ignores": ["**/*.gs.ts", "dist", "vendor"],
+    ignores: ['**/*.gs.ts', 'dist', 'vendor'],
   },
   {
-    "rules": {
-        "@typescript-eslint/no-explicit-any": "warn"
-    }
-  }
-);
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+    },
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'warn',
+    },
+  },
+)
