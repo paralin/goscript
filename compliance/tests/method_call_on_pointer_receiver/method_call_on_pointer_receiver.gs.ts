a8ac3daf72facb62575a84beaff6dca541732201
Christian Stewart <christian@aperture.us>
2025-05-06 15:54:05 -0700

    wipc
    Signed-off-by: Christian Stewart <christian@aperture.us>

--- compliance/tests/method_call_on_pointer_receiver/method_call_on_pointer_receiver.gs.ts
+++ compliance/tests/method_call_on_pointer_receiver/method_call_on_pointer_receiver.gs.ts
@@ -1,22 +1,42 @@
 // Generated file based on method_call_on_pointer_receiver.go
 // Updated when compliance tests are re-run, DO NOT EDIT!
 
-import * as goscript from "@goscript/builtin";
+import * as $ from "@goscript/builtin";
 
 class MyStruct {
-	public MyInt: number
-	public MyString: string
+	public get MyInt(): number {
+		return this._fields.MyInt.value
+	}
+	public set MyInt(value: number) {
+		this._fields.MyInt.value = value
+	}
+
+	public get MyString(): string {
+		return this._fields.MyString.value
+	}
+	public set MyString(value: string) {
+		this._fields.MyString.value = value
+	}
+
+	public _fields: {
+		MyInt: $.Box<number>;
+		MyString: $.Box<string>;
+	}
 
 	constructor(init?: Partial<{MyInt?: number, MyString?: string}>) {
-		this.MyInt = init?.MyInt ?? 0
-		this.MyString = init?.MyString ?? ""
+		this._fields = {
+			MyInt: $.box(init?.MyInt ?? 0),
+			MyString: $.box(init?.MyString ?? "")
+		}
 	}
 
 	public clone(): MyStruct {
-		return new MyStruct({
-			MyInt: this.MyInt,
-			MyString: this.MyString,
-		})
+		const cloned = new MyStruct()
+		cloned._fields = {
+			MyInt: $.box(this._fields.MyInt.value),
+			MyString: $.box(this._fields.MyString.value)
+		}
+		return cloned
 	}
 
 	// GetMyString returns the MyString field.
@@ -26,9 +46,9 @@ class MyStruct {
 	}
 
 	// Register this type with the runtime type system
-	static __typeInfo = goscript.registerType(
+	static __typeInfo = $.registerType(
 	  'MyStruct',
-	  goscript.TypeKind.Struct,
+	  $.TypeKind.Struct,
 	  new MyStruct(),
 	  new Set(['GetMyString']),
 	  MyStruct
@@ -36,7 +56,7 @@ class MyStruct {
 }
 
 export function main(): void {
-	let structPointer: goscript.Box<MyStruct> | null = goscript.box(new MyStruct({MyInt: 4, MyString: "hello world"}))
+	let structPointer = new MyStruct({MyInt: 4, MyString: "hello world"})
 	// === Method Call on Pointer Receiver ===
 	// Calling a method with a pointer receiver (*MyStruct) using a pointer variable.
 	console.log("Method call on pointer (structPointer): Expected: hello world, Actual: " + structPointer.GetMyString())
